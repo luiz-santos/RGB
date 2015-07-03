a8b2c30010c6da9946ff777773e3202840020de1
joaomarcoscmaciel <joaomarcos.cm.bh@gmail.com>
2015-07-03 17:30:14 -0300

    Rebuild + tentativa de animacao
    Rebuild + tentativa de animacao

--- RGB Quest/Assets/Scripts/PlayerController.cs
+++ RGB Quest/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,185 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
+	
+	public Grid grid;
+	public Celula.CellType playerColor;
+	bool canMove = false;
+	
+	public Animator anim;
+	
+	public int i, j;
+	
+	private int[] shift_i = {-1, 0, 1, 0};
+	private int[] shift_j = {0, 1, 0, -1};
+	
+	private Utils.Pair<int, int>[] movement;
+	private bool[,] marked;
+	private int totalMovements, currentMov;
+	
+	private int pMove = 180;
+	
+	void Awake(){
+		
+		anim = GetComponent <Animator> ();
+	}
+	
+	void Start () {
+		playerColor = Celula.CellType.R;
+		Utils.Pair<Vector3, float> aux = grid.getPlayerPositionAndRotation ();
+		transform.position = aux.first;
+		transform.Rotate (Vector3.up * aux.second);
+		
+		bool found = false;
+		for (i = 0; i < Grid.MAX_SIZE; i++) {
+			for (j = 0; j < Grid.MAX_SIZE; j++) {
+				if (grid.getCelula(i, j).getCellType() == Celula.CellType.Entrada) {
+					found = true;
+					break;
+				}
+			}
+			if (found) break;
+		}
+		
+		marked = new bool[Grid.MAX_SIZE, Grid.MAX_SIZE];
+		movement = new Utils.Pair<int, int>[Grid.MAX_SIZE * Grid.MAX_SIZE];
+		totalMovements = 0;
+		currentMov = 0;
+	}
+	
+	
+	
+	//Ativado pelo botao Start
+	public void releaseMovement() {
+		canMove = true;
+		
+		while (grid.getCelula(i,j).getCellType() != Celula.CellType.Saida) {
+			for (int k = 0; k < 4; k++) {
+				int I = i + shift_i[k], J = j + shift_j[k];
+				
+				if (I >= 0 && I < Grid.MAX_SIZE 
+				    && J >= 0 && J < Grid.MAX_SIZE 
+				    && !marked[I,J] 
+				    && (grid.getCelula(I,J).getCellType() == playerColor 
+				    || grid.getCelula(I,J).getCellType() == Celula.CellType.Saida)) {
+					
+					marked[I,J] = true;
+					movement[totalMovements] = new Utils.Pair<int, int>(I, J);
+					totalMovements++;
+					i = I;
+					j = J;
+					break;
+				}
+				
+			}
+		}
+	}
+	
+	void smoothRotatePlayer(int dMove){
+		if (dMove != pMove) {
+			for (int i = dMove - pMove; i <= dMove; i++) {
+				//Debug.Log (i);
+				transform.rotation = Quaternion.Euler (0, i, 0);
+				//yield WaitForSeconds(0.001F);
+				//System.Threading.Thread.Sleep(1);
+				//transform.Rotate(0, 1 * Time.deltaTime, 0);
+				//var target = Quaternion.Euler (0, i, 0);
+				// Dampen towards the target rotation
+				//transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime);
+			}
+		}
+	}
+	
+	int rotatePlayer(int x_p, int y_p, int x, int y){
+		int dMove = 0;
+		
+		if (y > y_p){
+			//Debug.Log ("TRAS");
+			dMove = 0;
+			smoothRotatePlayer(dMove);
+			//transform.Rotate(Vector3.back * Time.deltaTime);
+			
+		}
+		else if (x > x_p){
+			//Debug.Log ("ESQUERDA");
+			dMove = 90;
+			smoothRotatePlayer(dMove);
+			//transform.Rotate(Vector3.left * Time.deltaTime);
+		}
+		else if (x < x_p){
+			//Debug.Log ("DIREITA");
+			dMove = 270;
+			smoothRotatePlayer(dMove);
+			//transform.Rotate(Vector3.right * Time.deltaTime);
+		}
+		else if(y < y_p){
+			//Debug.Log ("FRENTE");
+			dMove = 180;
+			smoothRotatePlayer(dMove);
+			//transform.Rotate(Vector3.forward * Time.deltaTime);
+		}
+		
+		pMove = dMove;
+		return pMove;
+	}
+	
+	// Update is called once per frame
+	
+	void Update () {
+		if (!canMove || currentMov == totalMovements) {
+			//anim.SetBool ("IsWalking", false);
+			//Debug.Log ("idle");
+			return;
+		} else {
+			//anim.SetBool ("IsWalking", true);
+			//Debug.Log("walking");
+		}
+		
+		Transform nextTransform;
+		
+		if (currentMov == totalMovements - 1)
+			nextTransform = grid.exitPlatform.transform;
+		else {
+			Utils.Pair<int, int> p = movement[currentMov];
+			nextTransform = grid.getCelula(p.first, p.second).getGameObject().transform;
+		}
+		
+		if (nextTransform == null)
+			return;
+		
+		// Quando chega no pivo da celula
+		if (transform.position == nextTransform.position) {
+			
+			// rotacionar o personagem
+			int p_currentMov;
+			Utils.Pair<int, int> p = movement[currentMov];
+			
+			if(currentMov == totalMovements - 1)
+				rotatePlayer(0, 1, 0, 0);
+			else{
+				p_currentMov = currentMov + 1;
+				Utils.Pair<int, int> p_p = movement[p_currentMov];
+				pMove = rotatePlayer(p.first, p.second, p_p.first, p_p.second);
+			}
+			// fim rotacao
+			
+			grid.getCelula (p.first, p.second).nextColor ();
+			currentMov++;
+			
+		}
+		
+		//"Animacao" entre duas celulas
+		transform.position = Vector3.MoveTowards(transform.position, nextTransform.position, Time.deltaTime * 3);
+		
+	} 
+}
+
+
+/*using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
 
 	public Grid grid;
 	public Celula.CellType playerColor;
@@ -22,6 +201,8 @@ public class PlayerController : MonoBehaviour {
 	private Vector3 nextPosition = Vector3.zero;
 	private Utils.Pair<int, int> cellToChange;
 	private bool canChangeColor = false;
+
+	private int pMove = 180;
 
 	void Awake(){
 		
@@ -83,26 +264,6 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-	void rotatePlayer(int x_p, int y_p, int x, int y){
-		
-		if (y > y_p){
-			//Debug.Log ("TRAS");
-			transform.rotation = Quaternion.Euler (0, 0, 0);
-		}
-		else if (x > x_p){
-			//Debug.Log ("ESQUERDA");
-			transform.rotation = Quaternion.Euler(0, 90, 0);
-		}
-		else if (x < x_p){
-			//Debug.Log ("DIREITA");
-			transform.rotation = Quaternion.Euler(0, 270, 0);
-		}
-		else if(y < y_p){
-			//Debug.Log ("FRENTE");
-			transform.rotation = Quaternion.Euler(0, 180, 0);
-		}
-	}
-
 	// Update is called once per frame
 
 	/*void Update () {
@@ -153,8 +314,8 @@ public class PlayerController : MonoBehaviour {
 		
 	} */
 
-	void Update () {
-		if (!canMove || currentMov == totalMovements)
+	/*void Update () {
+		if (!canMove || currentMov == totalMovements) {
 			return;
 
 		if (currentMov == 0) {
@@ -196,4 +357,4 @@ public class PlayerController : MonoBehaviour {
 		//Interpolaçao do movimento entre duas celulas
 		transform.position = Vector3.MoveTowards(transform.position, nextPosition, Time.deltaTime * 3);
 	}
-}
+}*/
